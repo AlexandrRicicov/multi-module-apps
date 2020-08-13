3fee48f19bc471ea678cce407493cf53adb84949
Aexandr Ricicov <alexandr.ricicov@codefactorygroup.com>
2020-08-13 18:53:25 +0300

    git push origin master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counterapp1';
+
+
+  count$: Observable<number>
+
+  constructor(private store: Store<{ count: number }>) {
+    // TODO: This stream will connect to the current store `count` state
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    // TODO: Dispatch an increment action
+  }
+
+  decrement() {
+    // TODO: Dispatch a decrement action
+  }
+
+  reset() {
+    // TODO: Dispatch a reset action
+  }
 }
